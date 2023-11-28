98425835e95f4838eec4ea4f67da4024093f6451
Oleksandr Rohachev <oleksandr.rohachev@wework.com>
2023-11-28 17:06:53 +0200

    Added Zeitwerk autoloading

--- lib/rage/setup.rb
+++ lib/rage/setup.rb
@@ -1,5 +1,11 @@
 Iodine.patch_rack
 
 require_relative "#{Rage.root}/config/environments/#{Rage.env}"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.new
+loader.push_dir("#{Rage.root}/app")
+loader.setup # ready
+
 
 require_relative "#{Rage.root}/config/routes"
