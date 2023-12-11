6e5866a3a689e667c0c54fa321f04a8bed483af3
Oleksandr Rohachev <oleksandr.rohachev@wework.com>
2023-12-11 22:54:38 +0200

    Enable reloading

--- lib/rage/setup.rb
+++ lib/rage/setup.rb
@@ -8,6 +8,7 @@ autoload_path = "#{Rage.root}/app"
 
 loader = Zeitwerk::Loader.new
 loader.push_dir(autoload_path)
+loader.enable_reloading
 loader.setup
 file_watcher = Filewatcher.new(autoload_path )
 Thread.new(file_watcher) { |fw| fw.watch { |_| loader.reload } }
